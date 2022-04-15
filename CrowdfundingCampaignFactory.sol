fcabe176acda38256488c77c0932c241ac8d55e4
enyx02 <40337494+enyx02@users.noreply.github.com>
2022-04-15 17:28:29 +0300

    Add files via upload

---
+++ CrowdfundingCampaignFactory.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) external {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() external view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor(uint minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+   function contribute() external payable {
+        require(msg.value > minimumContribution);
+
+        require(!approvers[msg.sender]);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string calldata description, uint value, address payable recipient) external restricted {
+        Request storage newRequest = requests.push();
+ 
+        newRequest.description= description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0; 
+    }
+
+    function approveRequest(uint index) external {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) external restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
